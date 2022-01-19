c5da3accbef024daad27927d88bb1c2044b51e72
blupine <skwint11@gmail.com>
2022-01-19 21:58:03 +0900

    simple component & fragment

--- blog/src/App.js
+++ blog/src/App.js
@@ -9,18 +9,13 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [likeCnt, likeCntWriter] = useState(0);
 
-  function titleChange(){
-    var newArr = [...글제목]; // deep copy
-    newArr[0] = '여자 코트 추천';
-    글제목변경(newArr);
-  }
+
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-      <button onClick={ titleChange }>button</button>
       <div className="list">
         <h3> {글제목[0]} <span onClick={ ()=>{ likeCntWriter(likeCnt+1) } }>👍🏻</span> {likeCnt}</h3>
         <p>2월 17일 발행</p>
@@ -38,8 +33,28 @@ function App() {
         <p>2월 17일 발행</p>
         <hr/>
       </div>
+  
+      <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+  
+      <Modal>
+
+      </Modal>
+
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <>
+    <div className="modal"></div>
+    <div></div> 
+    </>
+  )
 }
 
 export default App;
