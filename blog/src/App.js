4660053549b0949d7d920e218b2018c9eb35ae79
blupine <skwint11@gmail.com>
2022-01-19 21:15:02 +0900

    simple state & description

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,21 +1,23 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let posts = {color : 'blue', fontSize : '30px'};
-  
-  function func(){
-    return 100
-  }
+  // destructuring
+  let [글제목, 글제목변경] = useState('남자 코트 추천');// state : 변수 대신 쓰는 데이터 저장 공간
   
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ posts }>개발 blog</div>
+        <div>개발 blog</div>
       </div>
-      <h4> { func() } </h4>
-      <img src={logo}></img>
+
+      <div className="list">
+        <h3> {글제목} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
