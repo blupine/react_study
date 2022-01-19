a5f57c2f48ba787176adc6f02390160d37dfe59a
blupine <skwint11@gmail.com>
2022-01-19 20:56:57 +0900

    simple function & data binding

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 function App() {
 
   let posts = '강남 고기 맛집';
-
+  
+  function func(){
+    return 100
+  }
+  
   return (
     <div className="App">
       <div className="black-nav">
         개발 blog
       </div>
       <h4> { posts } </h4>
+      <h4> { func() } </h4>
     </div>
   );
 }
