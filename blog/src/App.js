7adacc35d37a7cd50b31258a795695f7b10d1279
blupine <skwint11@gmail.com>
2022-01-19 21:20:29 +0900

    disable eslint-warning

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,3 +1,5 @@
+
+/* exlint-disable */
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -5,8 +7,11 @@ import './App.css';
 function App() {
 
   // destructuring
-  let [글제목, 글제목변경] = useState('남자 코트 추천');// state : 변수 대신 쓰는 데이터 저장 공간
-  
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  // state : 변수 대신 쓰는 데이터 저장 공간
+  // 그냥 변수는 변경되어도 새로고침이 되어야 재렌더링이 됨
+  // state는 변경되면 HTML이 자동으로 재렌더링 됨
+
   return (
     <div className="App">
       <div className="black-nav">
@@ -14,7 +19,19 @@ function App() {
       </div>
 
       <div className="list">
-        <h3> {글제목} </h3>
+        <h3> {글제목[0]} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      
+      <div className="list">
+        <h3> {글제목[1]} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+
+      <div className="list">
+        <h3> {글제목[2]} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
