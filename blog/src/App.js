5c8ec351b87f3e2fb451466ccc54fe9bfc2a6f87
blupine <skwint11@gmail.com>
2022-01-19 21:41:20 +0900

    using state and deep copy

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,18 +6,21 @@ import './App.css';
 
 function App() {
 
-  // destructuring
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
-  // state : 변수 대신 쓰는 데이터 저장 공간
-  // 그냥 변수는 변경되어도 새로고침이 되어야 재렌더링이 됨
-  // state는 변경되면 HTML이 자동으로 재렌더링 됨
   let [likeCnt, likeCntWriter] = useState(0);
+
+  function titleChange(){
+    var newArr = [...글제목]; // deep copy
+    newArr[0] = '여자 코트 추천';
+    글제목변경(newArr);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-
+      <button onClick={ titleChange }>button</button>
       <div className="list">
         <h3> {글제목[0]} <span onClick={ ()=>{ likeCntWriter(likeCnt+1) } }>👍🏻</span> {likeCnt}</h3>
         <p>2월 17일 발행</p>
