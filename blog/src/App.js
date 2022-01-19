a6f20db599740a3e78cb892a469331fa0bc1f0f4
blupine <skwint11@gmail.com>
2022-01-19 21:01:44 +0900

    using style(font size) on jsx

--- blog/src/App.js
+++ blog/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ {color : 'blue'} }>개발 blog</div>
+        <div style={ {color : 'blue', fontSize : '30px'} }>개발 blog</div>
       </div>
       <h4> { posts } </h4>
       <h4> { func() } </h4>
