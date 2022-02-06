a757d61a46db1db686d524f80b8d58b851f6b5e9
blupine <skwint11@gmail.com>
2022-02-07 00:27:25 +0900

    [1-6] 조건부 렌더링
    https://react.vlpt.us/basic/06-conditional-rendering.html
    삼항연산자, props 사용 추가

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -10,7 +10,9 @@ function App() {
     <Hello name="react" color="red" />
 
     {/* 여러개의 props 비구조화 할당 */}
-    <Hello2 name="react" color="red" />
+    <Hello2 name="react" color="red" isSpecial={true}/>
+    <Hello2 name="react" color="red" isSpecial/>
+
     
     {/* defaultProps로 기본값 설정 */}
     <Hello3 color="red"/>
--- begin-react/src/Hello2.js
+++ begin-react/src/Hello2.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function Hello2({color, name}){
-    return <div style={{color}}>안녕하세요. {name}</div>
+function Hello2({color, name, isSpecial}){
+    return <div style={{color}}>
+        {isSpecial ? <b>*</b> : null}
+        {isSpecial && <b>*</b>}
+        안녕하세요. {name}
+        </div>
 }
 
 export default Hello2;
