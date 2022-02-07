91f47d36b705a44e93d35c68622972d137e8eaf6
blupine <skwint11@gmail.com>
2022-02-07 23:50:19 +0900

    [1-16] useEffect를 사용하여 마운트/언마운트/업데이트시 할 작업 설정하기
    https://react.vlpt.us/basic/16-useEffect.html

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function User({ user, onRemove, onToggle}) {
+    useEffect(()=>{
+        // console.log("user 값이 설정됨");
+        console.log(user);
+        // return()=>{ // cleanup 함수
+        //     console.log("user 가 바뀌기 전..");
+        //     console.log(user);
+        // };
+    });
   return (
     <div>
       <b
