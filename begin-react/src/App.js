ab6618760231edccf2e83b195421273f697bce6d
blupine <skwint11@gmail.com>
2022-02-07 22:49:26 +0900

    [1-12] useRef로 컴포넌트 안의 변수 만들기
    https://react.vlpt.us/basic/12-variable-with-useRef.html

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import UserList from './UserList';
-import UserListMap from './UserListMap'
+
 function App() {
-  return (
-    <div>
-    <UserList/>  
-    <br></br>
-    <UserListMap/>
-    </div>
-  );    
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    nextId.current += 1;
+  };
+
+  return(
+    <UserList users={users} />
+  );
+
 }
 
 export default App;
