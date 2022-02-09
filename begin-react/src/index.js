16d07bd44e3b735b0f580b6b011ddf35777423f1
blupine <skwint11@gmail.com>
2022-02-09 23:28:25 +0900

    [1-20] 20. useReducer 를 사용하여 상태 업데이트 로직 분리하기 - Counter를 useReducer로 구현하기
    https://react.vlpt.us/basic/20-useReducer.html

--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Counter from './Counter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Counter></Counter>
   </React.StrictMode>,
   document.getElementById('root')
 );
